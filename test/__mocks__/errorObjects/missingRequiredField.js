af5fb7ff5eb42a127426388f1df66cad4642b222
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2021-01-30 10:52:06 +0100

    added more test mock objects for missingRequiredFields

--- test/__mocks__/errorObjects/missingRequiredField.js
+++ test/__mocks__/errorObjects/missingRequiredField.js
@@ -14,4 +14,46 @@ const missingData = {
   }
 };
 
-module.exports = { missingRule, missingData };
+const missingRuleField = {
+  rule: {
+    condition: 'gte',
+    condition_value: 30
+  },
+  data: {
+    name: 'John Smith',
+    age: 40,
+    email: 'johnsmith@example.com'
+  }
+};
+
+const missingRuleCondition = {
+  rule: {
+    field: 'age',
+    condition_value: 30
+  },
+  data: {
+    name: 'John Smith',
+    age: 40,
+    email: 'johnsmith@example.com'
+  }
+};
+
+const missingRuleConditionValue = {
+  rule: {
+    field: 'age',
+    condition: 'gte'
+  },
+  data: {
+    name: 'John Smith',
+    age: 40,
+    email: 'johnsmith@example.com'
+  }
+};
+
+module.exports = {
+  missingRule,
+  missingData,
+  missingRuleField,
+  missingRuleCondition,
+  missingRuleConditionValue
+};
